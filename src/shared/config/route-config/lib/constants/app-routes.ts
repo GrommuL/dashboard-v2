f72ac29ad4c1f10b17e2dc0455a3a275d75911c0
Grommul <miketsvg@gmail.com>
2023-10-23 15:53:59 +0600

    feat: добавил constants для route-config

---
+++ src/shared/config/route-config/lib/constants/app-routes.ts
@@ -0,0 +1,10 @@
+export enum AppRoutes {
+  MAIN = 'main',
+  DASHBOARD = 'dashboard',
+  BOOKINGS = 'bookings',
+  BOOKING = 'booking',
+  CABINS = 'cabins',
+  USERS = 'users',
+  SETTINGS = 'settings',
+  ACCOUNT = 'account'
+}
