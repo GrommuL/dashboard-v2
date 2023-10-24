54041c9a7ede7d106d2801c8b86a2d2bf8b35692
Grommul <miketsvg@gmail.com>
2023-10-24 13:31:59 +0600

    feat: реализовал смену темы

---
+++ src/features/theme/lib/theme.ts
@@ -0,0 +1,6 @@
+export enum ThemeVariants {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme'
---
+++ src/features/theme/model/use-theme.ts
@@ -0,0 +1,23 @@
+import { useContext, useState, Dispatch } from 'react'
+import { ThemeContext } from './theme-context'
+import { LOCAL_STORAGE_THEME_KEY, ThemeVariants } from '../lib/theme'
+
+interface UseThemeResult {
+  toggleTheme: () => void
+  theme: ThemeVariants
+}
+
+export const useTheme = (): UseThemeResult => {
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  const toggleTheme = () => {
+    const selectedTheme = theme === ThemeVariants.DARK ? ThemeVariants.LIGHT : ThemeVariants.DARK
+    setTheme(selectedTheme)
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, selectedTheme)
+  }
+
+  return {
+    theme,
+    toggleTheme
+  }
+}
