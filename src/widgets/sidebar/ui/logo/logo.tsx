496df17f768c63a55f2381c7d3e8d9ab36606561
Grommul <miketsvg@gmail.com>
2023-10-24 13:34:10 +0600

    feat: добавил logo

---
+++ src/widgets/sidebar/ui/logo/logo.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import { SiTreehouse } from 'react-icons/si'
+import { RoutePath } from 'shared/config/route-config'
+import style from './logo.module.scss'
+
+export const Logo = () => {
+  return (
+    <Link className={style.logo} to={RoutePath.main}>
+      <SiTreehouse size={36} />
+      <h1 className={style.title}>Dashboard</h1>
+    </Link>
+  )
+}
