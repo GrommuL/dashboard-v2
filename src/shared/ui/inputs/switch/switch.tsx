6fecb013497c13b458d04a305d54e32cc43501b9
Grommul <miketsvg@gmail.com>
2023-10-24 14:16:36 +0600

    update: добавил получение дополнительных стилей

--- src/shared/ui/inputs/switch/switch.tsx
+++ src/shared/ui/inputs/switch/switch.tsx
@@ -1,15 +1,22 @@
 import { FC, InputHTMLAttributes } from 'react'
 import style from './switch.module.scss'
+import clsx from 'clsx'
 
 interface SwitchProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string
+  className?: string
 }
 
-export const Switch: FC<SwitchProps> = ({ id, ...otherProps }) => {
+export const Switch: FC<SwitchProps> = ({ id, className, ...otherProps }) => {
   return (
     <div className={style.switch}>
       <label className={style.label} htmlFor={id} />
-      <input className={style.checkbox} {...otherProps} id={id} type='checkbox' />
+      <input
+        className={clsx(style.checkbox, [className])}
+        {...otherProps}
+        id={id}
+        type='checkbox'
+      />
     </div>
   )
 }
