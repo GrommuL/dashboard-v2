e82fbe80ad0ca8b3b343d1056ff06953571c6cfc
Grommul <miketsvg@gmail.com>
2023-10-25 12:39:04 +0600

    feat: добавил HotModuleReplacementPlugin

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,15 +1,24 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { ProgressPlugin, WebpackPluginInstance } from 'webpack'
+import {
+	ProgressPlugin,
+	WebpackPluginInstance,
+	DefinePlugin,
+	HotModuleReplacementPlugin
+} from 'webpack'
 import { BuildOptions } from './types/config'
 
-export const buildPlugins = ({ paths }: BuildOptions): WebpackPluginInstance[] => {
-  return [
-    new HTMLWebpackPlugin({ template: paths.html }),
-    new ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css'
-    })
-  ]
+export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+	return [
+		new HTMLWebpackPlugin({ template: paths.html }),
+		new ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css'
+		}),
+		new DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev)
+		}),
+		new HotModuleReplacementPlugin()
+	]
 }
