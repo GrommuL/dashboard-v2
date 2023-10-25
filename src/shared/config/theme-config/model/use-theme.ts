6e9b35fb79fa05a12f83c5b73e665661ce5a7c90
Grommul <miketsvg@gmail.com>
2023-10-25 12:49:07 +0600

    fix: исправление линтера

--- src/shared/config/theme-config/model/use-theme.ts
+++ src/shared/config/theme-config/model/use-theme.ts
@@ -11,8 +11,7 @@ export const useTheme = (): UseThemeResult => {
 	const { theme, setTheme } = useContext(ThemeContext)
 
 	const toggleTheme = () => {
-		const selectedTheme =
-			theme === ThemeVariants.DARK ? ThemeVariants.LIGHT : ThemeVariants.DARK
+		const selectedTheme = theme === ThemeVariants.DARK ? ThemeVariants.LIGHT : ThemeVariants.DARK
 		setTheme(selectedTheme)
 		localStorage.setItem(LOCAL_STORAGE_THEME_KEY, selectedTheme)
 	}
