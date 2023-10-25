ff83fed048e4f56acf430e6694540e34ce242664
Grommul <miketsvg@gmail.com>
2023-10-25 12:44:42 +0600

    update: подключил i18next

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,16 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './providers/app-router'
 import { ThemeProvider } from './providers/theme-provider'
 import './styles/global.scss'
+import '../shared/config/i18n-config'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <AppRouter />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider>
+				<AppRouter />
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 )
