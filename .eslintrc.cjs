306ff535d1edc2816813d024496b6b835337aff1
Grommul <miketsvg@gmail.com>
2023-10-27 15:38:43 +0600

    feat: добавил плагин storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,18 +5,19 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
-		'prettier'
+		'prettier',
+		'plugin:storybook/recommended'
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh', 'prettier'],
+	plugins: ['react-refresh', 'prettier', 'plugin:storybook/recommended'],
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		'@typescript-eslint/no-unused-vars': 'off',
-		'prettier/prettier': ['error', { endOfLine: 'auto', printWidth: 100 }],
-		'i18next/no-literal-string': [
-			'error',
-			{ markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }
-		]
+		'prettier/prettier': ['error', { endOfLine: 'auto', printWidth: 100 }]
+		// 'i18next/no-literal-string': [
+		// 	'error',
+		// 	{ markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }
+		// ]
 	}
 }
