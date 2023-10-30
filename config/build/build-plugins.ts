d67fa2ccf72744149008e0464a04e064471a7947
Grommul <miketsvg@gmail.com>
2023-10-30 17:30:11 +0600

    update: добавил плагин Dotenv

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -1,5 +1,6 @@
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import Dotenv from 'dotenv-webpack'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import {
 	ProgressPlugin,
@@ -9,7 +10,12 @@ import {
 } from 'webpack'
 import { BuildOptions } from './types/config'
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+export const buildPlugins = ({
+	paths,
+	isDev,
+	envFile,
+	NODE_ENV
+}: BuildOptions): WebpackPluginInstance[] => {
 	const plugins = [
 		new HTMLWebpackPlugin({ template: paths.html }),
 		new ProgressPlugin(),
@@ -17,8 +23,12 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInsta
 			filename: 'css/[name].[contenthash:8].css',
 			chunkFilename: 'css/[name].[contenthash:8].css'
 		}),
+		new Dotenv({
+			path: envFile
+		}),
 		new DefinePlugin({
-			__IS_DEV__: JSON.stringify(isDev)
+			__IS_DEV__: JSON.stringify(isDev),
+			'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
 		})
 	]
 
