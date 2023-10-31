e8e013aeb251715ee5c98adf49243332799974cd
Grommul <miketsvg@gmail.com>
2023-10-31 14:12:51 +0600

    update: добавил верстку страницы и cabin-table

--- src/pages/cabins/ui/cabins-page.tsx
+++ src/pages/cabins/ui/cabins-page.tsx
@@ -1,5 +1,21 @@
+import { useTranslation } from 'react-i18next'
+import { CabinTable } from 'widgets/cabin-table'
+import style from './cabins-page.module.scss'
+
 const CabinsPage = () => {
-	return <div>CabinsPage</div>
+	const { t } = useTranslation('cabins')
+
+	return (
+		<div className={style.cabins}>
+			<div className={style.cabinsHeader}>
+				<h2 className={style.heading}>{t('title')}</h2>
+				<div className={style.menu}>Filter/Sort</div>
+			</div>
+			<div className={style.row}>
+				<CabinTable />
+			</div>
+		</div>
+	)
 }
 
 export default CabinsPage
