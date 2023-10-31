c1f454620be5dc479b19de60bacbcaa53b5f6a96
Grommul <miketsvg@gmail.com>
2023-10-31 14:11:02 +0600

    feat: добавил cabins-type

---
+++ src/entities/cabins/types/cabins-type.ts
@@ -0,0 +1,10 @@
+export interface CabinType {
+	id?: number
+	name: string
+	description: string
+	maxCapacity: number
+	regularPrice: number
+	discount: number
+	image?: string
+	createdAt?: number
+}
