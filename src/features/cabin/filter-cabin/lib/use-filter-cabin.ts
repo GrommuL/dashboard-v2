10a510df669a8db12bdcf3926905f6052db3e3ab
Grommul <miketsvg@gmail.com>
2023-11-02 12:16:53 +0600

    feat: добавил сортировку

--- src/features/cabin/filter-cabin/lib/use-filter-cabin.ts
+++ src/features/cabin/filter-cabin/lib/use-filter-cabin.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { getCabins } from 'entities/cabins'
+import { CabinType, getCabins } from 'entities/cabins'
 import { useSearchParams } from 'react-router-dom'
 
 export const useFilterCabin = () => {
@@ -28,9 +28,29 @@ export const useFilterCabin = () => {
 		}
 	})
 
+	const sortByValue = searchParams.get('sortBy') || 'name-asc'
+	const [field, direction] = sortByValue.split('-') as [
+		keyof Pick<CabinType, 'name' | 'regularPrice' | 'maxCapacity'>,
+		string
+	]
+	const modifier = direction === 'asc' ? 1 : -1
+	const sortedCabins =
+		filteredCabins &&
+		filteredCabins?.sort((a, b) => {
+			if (field === 'name') {
+				if (direction === 'asc') {
+					return a[field].localeCompare(b[field])
+				} else {
+					return b[field].localeCompare(a[field])
+				}
+			}
+			return (a[field] - b[field]) * modifier
+		})
+
 	return {
 		cabins,
 		filteredCabins,
+		sortedCabins,
 		isLoading,
 		error,
 		refetch
