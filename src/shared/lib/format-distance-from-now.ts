17063c1c700d561d8d2f35fa8635c4234e0a6213
Grommul <miketsvg@gmail.com>
2023-11-09 16:34:49 +0600

    fix: удалил закомментаренный код

--- src/shared/lib/format-distance-from-now.ts
+++ src/shared/lib/format-distance-from-now.ts
@@ -1,11 +1,5 @@
 import { formatDistance, parseISO } from 'date-fns'
 
-// export const formatDistanceFromNow = (timestamp: number): string => {
-// 	const date = new Date(timestamp)
-// 	const distance = formatDistanceToNow(date, { addSuffix: true })
-// 	return distance.replace('in ', '').replace('in', 'In')
-// }
-
 export const formatDistanceFromNow = (dateStr: string) =>
 	formatDistance(parseISO(dateStr), new Date(), {
 		addSuffix: true
