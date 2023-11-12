8a0f28eb8ec8296e97151331ba9ad104b7914660
Grommul <miketsvg@gmail.com>
2023-11-12 13:15:52 +0600

    update: добавил update-settings

--- src/pages/settings/ui/settings-page.tsx
+++ src/pages/settings/ui/settings-page.tsx
@@ -1,5 +1,15 @@
+import { UpdateSettings } from 'features/settings/update-settings'
+import style from './settings-page.module.scss'
+
 const SettingsPage = () => {
-	return <div>SettingsPage</div>
+	return (
+		<main>
+			<h1 className={style.title}>Update hotel settings</h1>
+			<div className={style.form}>
+				<UpdateSettings />
+			</div>
+		</main>
+	)
 }
 
 export default SettingsPage
