1f5fee4ef240f2ed33c0b1be2f05625b476ac01b
Grommul <miketsvg@gmail.com>
2023-11-14 17:18:38 +0600

    feat: добавил guest-type

---
+++ src/entities/guests/index.ts
@@ -0,0 +1,2 @@
+export { GuestType } from './lib/guest-type'
+export { getGuest } from './api/guests-api'
---
+++ src/entities/guests/lib/guest-type.ts
@@ -0,0 +1,9 @@
+export type GuestType = {
+	id: number
+	fullName: string
+	email: string
+	nationality: string
+	nationalID: string
+	countryFlag: string
+	createdAt: number
+}
