4a0732c0a2b1dee24201792ec52463dc74436c64
Grommul <miketsvg@gmail.com>
2023-11-15 15:06:09 +0600

    update: добавил перевод

--- src/features/pagination/ui/pagination.tsx
+++ src/features/pagination/ui/pagination.tsx
@@ -3,6 +3,7 @@ import style from './pagination.module.scss'
 import { FC } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { Button } from 'shared/ui/buttons/button'
+import { useTranslation } from 'react-i18next'
 
 interface PaginationProps {
 	count: number
@@ -11,6 +12,7 @@ interface PaginationProps {
 }
 
 export const Pagination: FC<PaginationProps> = ({ count, pageSize, currentPage }) => {
+	const { i18n, t } = useTranslation()
 	const [searchParams, setSearchParams] = useSearchParams()
 
 	const pageCount = Math.ceil(count / pageSize)
@@ -32,17 +34,26 @@ export const Pagination: FC<PaginationProps> = ({ count, pageSize, currentPage }
 
 	return (
 		<div className={style.pagination}>
-			<p className={style.info}>
-				Showing <span>{(currentPage - 1) * pageSize + 1}</span> to{' '}
-				<span>{currentPage === pageCount ? count : currentPage * pageSize}</span> of{' '}
-				<span>{count}</span> results
-			</p>
+			{i18n.language === 'ru' ? (
+				<p className={style.info}>
+					Показаны результаты с <span>{(currentPage - 1) * pageSize + 1}</span> по{' '}
+					<span>{currentPage === pageCount ? count : currentPage * pageSize}</span> из{' '}
+					<span>{count}</span>
+				</p>
+			) : (
+				<p className={style.info}>
+					Showing <span>{(currentPage - 1) * pageSize + 1}</span> to{' '}
+					<span>{currentPage === pageCount ? count : currentPage * pageSize}</span> of{' '}
+					<span>{count}</span> results
+				</p>
+			)}
+
 			<div className={style.buttons}>
 				<Button variant='empty' onClick={previousPage} disabled={currentPage === 1}>
-					<HiChevronLeft /> Previous
+					<HiChevronLeft /> {t('pagination.prev-button')}
 				</Button>
 				<Button variant='empty' onClick={nextPage} disabled={currentPage === pageCount}>
-					Next <HiChevronRight />
+					{t('pagination.next-button')} <HiChevronRight />
 				</Button>
 			</div>
 		</div>
