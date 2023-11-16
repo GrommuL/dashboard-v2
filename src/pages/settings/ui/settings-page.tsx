2c4446efecac6f53e5e17a93f68c69812594c95a
Grommul <miketsvg@gmail.com>
2023-11-16 14:57:51 +0600

    feat: добавил перевод для settings-page

--- src/pages/settings/ui/settings-page.tsx
+++ src/pages/settings/ui/settings-page.tsx
@@ -1,10 +1,12 @@
 import { UpdateSettings } from 'features/settings/update-settings'
 import style from './settings-page.module.scss'
+import { useTranslation } from 'react-i18next'
 
 const SettingsPage = () => {
+	const { t } = useTranslation('settings')
 	return (
 		<main>
-			<h1 className={style.title}>Update hotel settings</h1>
+			<h1 className={style.title}>{t('settings-page.title')}</h1>
 			<div className={style.form}>
 				<UpdateSettings />
 			</div>
