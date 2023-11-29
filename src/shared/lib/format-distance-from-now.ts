6d37ce3165c6a397dbbc69255c88446844492b61
Grommul <miketsvg@gmail.com>
2023-11-29 15:01:05 +0600

    feat: добавил переводы

--- src/shared/lib/format-distance-from-now.ts
+++ src/shared/lib/format-distance-from-now.ts
@@ -1,8 +1,17 @@
 import { formatDistance, parseISO } from 'date-fns'
+import { enUS, ru } from 'date-fns/locale'
+import { useTranslation } from 'react-i18next'
 
-export const formatDistanceFromNow = (dateStr: string) =>
-	formatDistance(parseISO(dateStr), new Date(), {
-		addSuffix: true
-	})
-		.replace('about ', '')
-		.replace('in', 'In')
+export const useFormatDistanceFromNow = () => {
+	const { i18n } = useTranslation()
+
+	const formatDistanceFromNow = (dateStr: string) =>
+		formatDistance(parseISO(dateStr), new Date(), {
+			addSuffix: true,
+			locale: i18n.language === 'en' ? enUS : ru
+		})
+			.replace('about ', '')
+			.replace('in', 'In')
+
+	return { formatDistanceFromNow }
+}
