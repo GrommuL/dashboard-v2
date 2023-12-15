aac998895e27d5af4936a1add370e9ed71953267
Grommul <miketsvg@gmail.com>
2023-12-15 15:13:41 +0600

    feat: добавил переводы для страницы guests

--- src/pages/guests/ui/guests-page.tsx
+++ src/pages/guests/ui/guests-page.tsx
@@ -1,11 +1,14 @@
 import { GuestsTable } from 'widgets/guests-table'
 import style from './guests-page.module.scss'
+import { useTranslation } from 'react-i18next'
 
 const GuestsPage = () => {
+	const { t } = useTranslation('guests')
+
 	return (
 		<div className={style.guests}>
 			<div className={style.guestsHeader}>
-				<h2 className={style.heading}>Guests</h2>
+				<h2 className={style.heading}>{t('title')}</h2>
 			</div>
 			<div className={style.row}>
 				<GuestsTable />
