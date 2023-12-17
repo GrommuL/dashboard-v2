70d185f7f85f15be902fe53e7ac97d22cef86674
Grommul <miketsvg@gmail.com>
2023-12-17 15:35:42 +0600

    feat: добавил создание и удаление гостя

--- src/entities/guests/api/guests-api.ts
+++ src/entities/guests/api/guests-api.ts
@@ -10,3 +10,21 @@ export const getGuests = async () => {
 	const { data } = await instance.get<GuestType[]>('guests')
 	return data
 }
+
+export const createGuest = async (guest: GuestType) => {
+	try {
+		await instance.post('guests', guest)
+	} catch (error) {
+		console.log(error)
+		throw new Error('Guest could not be created')
+	}
+}
+
+export const deleteGuest = async (id: number) => {
+	try {
+		await instance.delete(`guests/${id}`)
+	} catch (error) {
+		console.log(error)
+		throw new Error('Cabin could not be deleted')
+	}
+}
